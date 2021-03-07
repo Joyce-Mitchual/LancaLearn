49e752d00158dcf837df6f258297c8ecb3e8e45d
Joyce-Mitchual <joycemitchual19@gmail.com>
2021-03-07 17:12:00 +0000

    initial commit

---
+++ app/src/main/java/com/example/grpwork/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.grpwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static int LANCALEARN_SCREEN=5000;
+
+    //Variables
+    Animation topAmin, bottomAmin;
+    ImageView image;
+    TextView logo, groupname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //Animation
+        topAmin= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAmin= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        image= findViewById(R.id.imageView);
+        logo= findViewById(R.id.textView);
+        groupname= findViewById(R.id.textView2);
+
+        image.setAnimation(topAmin);
+        logo.setAnimation(bottomAmin);
+        groupname.setAnimation(bottomAmin);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Dashboard.class);
+                Pair[ ]pairs=new Pair[2];
+                pairs[0]=new Pair<View, String>(image,"logo_image");
+                pairs[1]=new Pair<View, String>(logo,"logo_text");
+
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                    startActivity(intent,options.toBundle());
+                }
+
+            }
+        }, LANCALEARN_SCREEN);
+
+    }
+}
---
+++ app/src/main/java/com/example/grpwork/SignUp.java
@@ -0,0 +1,230 @@
+package com.example.grpwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUp extends AppCompatActivity {
+
+    //Variables
+
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button regBtn, regToLoginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        //Hooks to find all xml element in activity_sign_up.xml
+
+        regName=findViewById(R.id.reg_name);
+        regUsername=findViewById(R.id.reg_username);
+        regEmail=findViewById(R.id.reg_email);
+        regPhoneNo=findViewById(R.id.reg_phoneNo);
+        regPassword=findViewById(R.id.reg_password);
+        regBtn=findViewById(R.id.reg_btn);
+        regToLoginBtn=findViewById(R.id.reg_login_btn);
+
+
+
+    }//Onclick Method end
+
+
+   /*
+    private boolean validateName(){
+        String val=regName.getEditText().getText().toString();
+
+        if (val.isEmpty()){
+
+            regName.setError("Field cannot be empty ");
+            return false;
+
+        }
+        else {
+            regName.setError(null);//hide the error
+            regName.setErrorEnabled(false);//remove space from the error
+            return true;
+        }
+
+
+    }
+
+    private boolean validateUsername(){
+        String val=regName.getEditText().getText().toString();
+        String noWhiteSpace="(?=\\s+$)";
+
+        if (val.isEmpty()){
+
+            regName.setError("Field cannot be empty ");
+            return false;
+
+        }
+
+        else if (val.length()>=15){
+            regName.setError("Username too long");
+            return false;
+        }
+
+        else if (!val.matches(noWhiteSpace)){
+            regName.setError("White Space are not allowed");
+            return false;
+        }
+        else {
+            regName.setError(null);
+
+            return true;
+        }
+
+
+    }
+
+    private boolean validateEmail(){
+        String val=regName.getEditText().getText().toString();
+        String emailPattern="[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]";
+
+
+        if (val.isEmpty()){
+
+            regName.setError("Field cannot be empty ");
+            return false;
+
+        }
+        else if(val.matches(emailPattern)){
+            regName.setError("Invaild email address");
+            return false;
+        }
+        else {
+            regName.setError(null);
+            return true;
+        }
+
+
+    }
+
+    private boolean validatePhoneNo(){
+        String val=regName.getEditText().getText().toString();
+
+        if (val.isEmpty()){
+
+            regName.setError("Field cannot be empty ");
+            return false;
+
+        }
+        else {
+            regName.setError(null);
+            return true;
+        }
+
+
+    }
+
+    private boolean validatePassword(){
+        String val=regPassword.getEditText().getText().toString();
+        String passwordVal="^"+
+               "(?=.*[A-Z])"+ //one upper case
+                "(?=.*[a-zA=Z])" + //any letter
+                "(?=\\s+$)"+    //no whitespace
+                ".{4,}"+ //at least four characters
+                "$";
+
+        if (val.isEmpty()){
+
+            regName.setError("Field cannot be empty ");
+            return false;
+
+        }
+        else if(val.matches(passwordVal)){
+            regName.setError("Password is too weak");
+            return false;
+        }
+        else {
+            regName.setError(null);
+            return true;
+        }
+
+
+    }
+
+
+
+
+    */
+
+
+    public void registerUser(View view) {
+
+       // if (!validateName() | !validatePassword()| !validatePhoneNo() | !validateEmail()| !validateUsername() ){
+         //   return;
+       // }
+
+
+
+
+        rootNode= FirebaseDatabase.getInstance();
+        reference=rootNode.getReference("users");
+
+        //get all the values
+        String name=regName.getEditText().getText().toString();
+        String username=regUsername.getEditText().getText().toString();
+        String email=regEmail.getEditText().getText().toString();
+        String phoneNo=regPhoneNo.getEditText().getText().toString();
+        String password=regPassword.getEditText().getText().toString();
+
+
+        UserHelperClass helperClass=new UserHelperClass(name,username,email,phoneNo,password);
+
+        reference.child(username).setValue(helperClass);
+
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+      /*  regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+
+                //get all the values
+                String name=regName.getEditText().getText().toString();
+                String username=regUsername.getEditText().getText().toString();
+                String email=regEmail.getEditText().getText().toString();
+                String phoneNo=regPhoneNo.getEditText().getText().toString();
+                String password=regPassword.getEditText().getText().toString();
+
+
+                UserHelperClass helperClass=new UserHelperClass(name,username,email,phoneNo,password);
+
+                reference.child(phoneNo).setValue(helperClass);
+
+
+
+            }
+        });  //register Button method
+
+
+       */
